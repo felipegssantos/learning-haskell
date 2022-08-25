e2a0af7c10f39e94c0bedb9aee35ebc5efa58110
Felipe Santos <felipegssantos@gmail.com>
2022-08-25 15:17:00 -0300

    Insert messages in a timestamp sorted binary tree

--- 02-ADTs/LogAnalysis.hs
+++ 02-ADTs/LogAnalysis.hs
@@ -21,3 +21,14 @@ parseMessage' other = Unknown (unwords other)
 parse :: String -> [LogMessage]
 parse logMessages = map parseMessage (lines logMessages)
 
+-- insert a log message into an ordered binary search tree
+insert :: LogMessage -> MessageTree -> MessageTree
+insert msg@(LogMessage _ t _) (Node left msg'@(LogMessage _ t' _) right)
+  | t < t' = Node (insert msg left) msg' right
+  | t > t' = Node left msg' (insert msg right)
+insert (Unknown _) tree = tree -- ignore unknown messages
+-- when a Leaf is reached, replace it with a new node
+insert msg Leaf = Node Leaf msg Leaf
+-- evaluate to nonsensical value for unexpected inputs?
+insert _ _ = (Node Leaf (Unknown "") Leaf)
+
