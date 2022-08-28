ee3c519d221c4cc485750b7a20f8b9818de568b6
Felipe Santos <felipegssantos@gmail.com>
2022-08-28 00:08:52 -0300

    First try at building histogram function

--- 03-rec-poly/Golf.hs
+++ 03-rec-poly/Golf.hs
@@ -42,3 +42,55 @@ localMaxima (x:y:z:xs)
   | otherwise = localMaxima (y:z:xs)
 localMaxima _ = []
 
+
+{-
+Exercise 3: Histogram
+-}
+
+-- TODO: make transpose safe by means of the type system
+
+-- transpose a list of lists
+transpose :: a -> [[a]] -> [[a]]
+transpose _ ([]:_) = [] -- avoids that the default value def is ever used
+transpose def xs = (map (headDef def) xs) : (transpose def (map tailSafe xs))
+
+-- safe way to get the head of a list and returning a default value in case
+-- the list is empty
+headDef :: a -> [a] -> a
+headDef def [] = def -- does not really matter; transpose never reaches it
+headDef _ (x:_) = x
+
+-- safe way to get the tail of list, defaulting to an empty list in case
+-- the input is empty
+tailSafe :: [a] -> [a] 
+tailSafe [] = [] -- does not really matter; transpose never reaches it
+tailSafe (_:xs) = xs
+
+histogram :: [Integer] -> String
+histogram digits = let xAxis = [replicate 10 '=', "0123456789"]
+                       histBars = countsToStrings . countDigits
+                   in unlines $ (transpose '?' (histBars digits))
+                                ++ xAxis
+
+countsToStrings :: [Integer] -> [String]
+countsToStrings counts
+  = let size = maximumSafe counts
+    in map (\count -> 
+             (replicate (fromIntegral (size - count)) ' ')
+             ++ (replicate (fromIntegral count) '*'))
+           counts
+
+-- a safe way to get the maximum among (non-negative) integers
+maximumSafe :: [Integer] -> Integer
+maximumSafe [] = 0
+maximumSafe xs = maximum xs
+
+-- counts digits in a list
+countDigits :: [Integer] -> [Integer]
+countDigits digits = foldl countOneDigit (replicate 10 0) digits
+
+countOneDigit :: [Integer] -> Integer -> [Integer]
+countOneDigit currentCounts n
+  = map (\(count, i) -> if i == n then count + 1 else count)
+        (zip currentCounts [0..9])
+
