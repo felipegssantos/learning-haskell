d8372112db48bad3392202d10a8ab8043267a19f
Felipe Santos <felipegssantos@gmail.com>
2022-08-30 10:53:17 -0300

    Build a binary tree using foldr

--- 04-higher-order/Assignments.hs
+++ 04-higher-order/Assignments.hs
@@ -27,3 +27,33 @@ fun2' = sum . filter even . takeWhile (>1) . iterate next
 next :: Integer -> Integer
 next n = if (even n) then (n `div` 2) else (3 * n + 1)
 
+{-
+Exercise 2: Folding with trees
+-}
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+-- inserts new element to a tree by attaching the new element to the shorter
+-- subtree of the root and then updating heights if necessary
+insert :: a -> Tree a -> Tree a
+insert new Leaf = Node 0 Leaf new Leaf
+insert new (Node h left old right)
+  | height left < height right = Node h (insert new left) old right
+  | height left > height right = Node h left old (insert new right)
+  | isFull left = Node (h + 1) (insert new left) old right
+  | otherwise = Node h (insert new left) old right
+
+height :: Tree a -> Integer
+height Leaf = -1 -- make sure leaves have height smaller than any node
+height (Node h _ _ _) = h
+
+isFull :: Tree a -> Bool
+isFull Leaf = True
+isFull (Node _ left _ right) = (height left == height right)
+                               && isFull left && isFull right
+
