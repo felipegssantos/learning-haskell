901c0fd071a7e670630e539161429c82d7e33548
Felipe Santos <felipegssantos@gmail.com>
2022-08-31 09:22:09 -0300

    Evaluate expressions given as strings

--- 05-type-classes/Calc.hs
+++ 05-type-classes/Calc.hs
@@ -3,6 +3,7 @@
 module Calc where
 
 import ExprT
+import Parser
 
 -- Exercise 1: evaluate expressions
 eval :: ExprT -> Integer
@@ -10,4 +11,10 @@ eval (Lit n) = n
 eval (Add n m) = (eval n) + (eval m)
 eval (Mul n m) = (eval n) * (eval m)
 
+-- Exercise 2: parse and evaluate
+evalStr :: String -> Maybe Integer
+evalStr = evalIfJust . (parseExp Lit Add Mul)
+
+evalIfJust :: Maybe ExprT -> Maybe Integer
+evalIfJust = maybe Nothing (\x -> Just (eval x))
 
