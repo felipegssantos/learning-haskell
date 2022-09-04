803fc9e0e7f6984af0bb8fdf6f25075f07516bbe
Felipe Santos <felipegssantos@gmail.com>
2022-09-03 21:45:24 -0300

    Build stream data type

--- 06-laziness/Fibonacci.hs
+++ 06-laziness/Fibonacci.hs
@@ -15,3 +15,12 @@ fibs1 = map fib [0..]
 fibs2 :: [Integer]
 fibs2 = map fst $ iterate (\(x, y) -> (y, x + y)) (0, 1)
 
+-- Exercise 3: define a stream
+data Stream a = Stream a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Stream x xs) = x : (streamToList xs)
+
+instance Show a => Show (Stream a) where
+  show stream = show $ take 10 (streamToList stream)
+
