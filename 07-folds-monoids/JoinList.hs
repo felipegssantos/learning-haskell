3e825ac3462ebb2559034fd10b2a28a052322ce0
Felipe Santos <felipegssantos@gmail.com>
2022-09-06 13:41:24 -0300

    Retrieve JoinList element by list-like index
    The indexJ operation is equivalent to turning the JoinList into a
    standard list and retrieve an element by its (zero-based) index.

--- 07-folds-monoids/JoinList.hs
+++ 07-folds-monoids/JoinList.hs
@@ -2,6 +2,8 @@
 
 module JoinList where
 
+import Sized
+
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
@@ -15,4 +17,18 @@ tag :: Monoid m => JoinList m a -> m
 tag Empty = mempty
 tag (Single m _) = m
 tag (Append m _ _) = m
+
+-- Exercise 2.1: lookup JoinList by index
+tagSize :: (Sized b, Monoid b) => JoinList b a -> Int
+tagSize = getSize . size . tag
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+indexJ n (Append b left right)
+  | n >= (getSize . size) b = Nothing
+  | tagSize left <= n = indexJ (n - tagSize left) right
+  | otherwise = indexJ n left
+indexJ n (Single _ a)
+  | n == 0 = Just a -- Single's have a single index: zero
+  | otherwise = Nothing
 
