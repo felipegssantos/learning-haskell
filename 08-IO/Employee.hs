96e62accdf037031e0e7f39d6b3487d522dd15c0
Felipe Santos <felipegssantos@gmail.com>
2022-09-09 14:06:18 -0300

    Fix excess spaces

--- 08-IO/Employee.hs
+++ 08-IO/Employee.hs
@@ -1,13 +1,13 @@
 module Employee where
 
-import           Data.Tree
+import Data.Tree
 
 -- Employee names are represented by Strings.
 type Name = String
 
 -- The amount of fun an employee would have at the party, represented
 -- by an Integer
-type Fun  = Integer
+type Fun = Integer
 
 -- An Employee consists of a name and a fun score.
 data Employee = Emp { empName :: Name, empFun :: Fun }
